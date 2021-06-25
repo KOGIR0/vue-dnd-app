402baa713241ce915a85cd9a469c9cb79d6abcdc
IGOR <kondrahov.2000@mail.ru>
2021-06-25 10:39:19 +0300

    Added CreateSpellObj and CreateMonsterObj functions

--- src/components/App.vue
+++ src/components/App.vue
@@ -20,6 +20,51 @@
 import ItemList from './ItemList.vue';
 import ObjectList from './ObjectList.vue';
 
+function CreateMonsterObj(data)
+{
+    return {
+        'Name': data.name,
+        'Armor Class': data.armor_class,
+        'Challenge Rating': data.challenge_rating,
+        'Charisma': data.charisma,
+        'Constitution': data.constitution,
+        'Damage Immunities': data.damage_immunities.join(','),
+        'Damage Resistances': data.damage_resistances.join(','),
+        'Damage Vulnerabilities': data.damage_vulnerabilities.join(','),
+        'Dexterity': data.dexterity,
+        'Hit Dice': data.hit_dice,
+        'Hit Points': data.hit_points,
+        'Intelligence': data.intelligence,
+        'Languages': data.languages,
+        'Sences': data.sences,
+        'Size': data.size,
+        'Strength': data.strength,
+        'Type': data.type,
+        'Wisdom': data.wisdom,
+        'XP': data.xp
+    }
+}
+
+function CreateSpellObj(data)
+{
+    return {
+        'Name': data.name,
+        'Area of effect': data.hasOwnProperty('area_of_effect') ? data.area_of_effect.type + ' ' + data.area_of_effect.size : '',
+        'Casting time': data.casting_time,
+        'Classes': data.classes.map((item) => item.name).join(','),
+        'Components': data.components.join(','),
+        'Concentration': data.concentration.toString(),
+        'Desctription': data.desc.join('.'),
+        'Duration': data.duration,
+        'Level': data.level,
+        'Material': data.material,
+        'Range': data.range,
+        'Ritual': data.ritual.toString(),
+        'School': data.school.name,
+        'Subclasses': data.subclasses.map((item) => item.name).join(',')
+    }
+}
+
 export default {
     data(){
         return {
@@ -56,46 +101,11 @@ export default {
         {
             let dataProcess = {
                 'monsters': (data) => {
-                    let monster = {
-                        'Name': data.name,
-                        'Armor Class': data.armor_class,
-                        'Challenge Rating': data.challenge_rating,
-                        'Charisma': data.charisma,
-                        'Constitution': data.constitution,
-                        'Damage Immunities': data.damage_immunities.join(','),
-                        'Damage Resistances': data.damage_resistances.join(','),
-                        'Damage Vulnerabilities': data.damage_vulnerabilities.join(','),
-                        'Dexterity': data.dexterity,
-                        'Hit Dice': data.hit_dice,
-                        'Hit Points': data.hit_points,
-                        'Intelligence': data.intelligence,
-                        'Languages': data.languages,
-                        'Sences': data.sences,
-                        'Size': data.size,
-                        'Strength': data.strength,
-                        'Type': data.type,
-                        'Wisdom': data.wisdom,
-                        'XP': data.xp
-                    }
+                    let monster = CreateMonsterObj(data);
                     this.chosenItemParam = monster;
                 },
                 'spells': (data) => {
-                    let spell = {
-                        'Name': data.name,
-                        'Area of effect': data.hasOwnProperty('area_of_effect') ? data.area_of_effect.type + ' ' + data.area_of_effect.size : '',
-                        'Casting time': data.casting_time,
-                        'Classes': data.classes.map((item) => item.name).join(','),
-                        'Components': data.components.join(','),
-                        'Concentration': data.concentration.toString(),
-                        'Desctription': data.desc.join('.'),
-                        'Duration': data.duration,
-                        'Level': data.level,
-                        'Material': data.material,
-                        'Range': data.range,
-                        'Ritual': data.ritual.toString(),
-                        'School': data.school.name,
-                        'Subclasses': data.subclasses.map((item) => item.name).join(',')
-                    }
+                    let spell = CreateSpellObj(data);
                     this.chosenItemParam = spell;
                 },
                 'classes': (data) => {
